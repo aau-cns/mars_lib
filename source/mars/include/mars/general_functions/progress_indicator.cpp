6706b885ecc63438588afc6a9bd8d33db60d9e28
Christian Brommer <christian.brommer@aau.at>
2021-11-21 21:49:44 +0100

    feat: added progress indicator tool

---
+++ source/mars/include/mars/general_functions/progress_indicator.cpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2021 Christian Brommer, Control of Networked Systems, University of Klagenfurt, Austria.
+//
+// All rights reserved.
+//
+// This software is licensed under the terms of the BSD-2-Clause-License with
+// no commercial use allowed, the full terms of which are made available
+// in the LICENSE file. No license in patents is granted.
+//
+// You can contact the author at <christian.brommer@ieee.org>
+
+#include "progress_indicator.h"
+namespace mars
+{
+ProgressIndicator::ProgressIndicator(const int& total_iterations, const int& step_width)
+  : total_iterations_(total_iterations), step_width_(step_width)
+{
+}
+
+void ProgressIndicator::next_step()
+{
+  current_iteration_++;
+  const double prog = (current_iteration_ / total_iterations_) * 100;
+  if (prog >= current_progress_)
+  {
+    std::cout << "Progress: [ ";
+    std::cout << std::setprecision(2) << current_progress_;
+    std::cout << "% ]" << std::endl;
+    current_progress_ += step_width_;
+  }
+}
+}
---
+++ source/mars/include/mars/general_functions/progress_indicator.h
@@ -0,0 +1,35 @@
+// Copyright (C) 2021 Christian Brommer, Control of Networked Systems, University of Klagenfurt, Austria.
+//
+// All rights reserved.
+//
+// This software is licensed under the terms of the BSD-2-Clause-License with
+// no commercial use allowed, the full terms of which are made available
+// in the LICENSE file. No license in patents is granted.
+//
+// You can contact the author at <christian.brommer@ieee.org>
+
+#ifndef PROGRESSINDICATOR_H
+#define PROGRESSINDICATOR_H
+
+#include <iomanip>
+#include <iostream>
+
+namespace mars
+{
+class ProgressIndicator
+{
+public:
+  ProgressIndicator(const int& total_iterations, const int& step_width);
+
+  void next_step();
+
+private:
+  const double total_iterations_;
+  const double step_width_;
+
+  double current_progress_{ 0 };
+  int current_iteration_{ 0 };
+};
+}
+
+#endif  // PROGRESSINDICATOR_H
