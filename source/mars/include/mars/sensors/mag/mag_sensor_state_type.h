e1cbade27e10f7af3ad9fd1ab4ec8e2f39565d45
Christian Brommer <christian.brommer@aau.at>
2021-12-20 11:26:35 +0100

    feat: added csv export and header export to gps and mag sensor

--- source/mars/include/mars/sensors/mag/mag_sensor_state_type.h
+++ source/mars/include/mars/sensors/mag/mag_sensor_state_type.h
@@ -29,6 +29,33 @@ public:
     mag_.setZero();
     q_im_.setIdentity();
   }
+
+  static std::string get_csv_state_header_string()
+  {
+    std::stringstream os;
+    os << "t, ";
+    os << "mag_w_x, mag_w_y, mag_w_z, ";
+    os << "q_im_w, q_im_x, q_im_y, q_im_z";
+
+    return os.str();
+  }
+
+  ///
+  /// \brief to_csv_string export state to single csv string
+  /// \param timestamp
+  /// \return string format [mag q_wi]
+  ///
+  std::string to_csv_string(const double& timestamp) const
+  {
+    std::stringstream os;
+    os << timestamp;
+
+    os << ", " << mag_(0) << ", " << mag_(1) << ", " << mag_(2);
+    Eigen::Vector4d q_im = q_im_.coeffs();  // x y z w
+    os << ", " << q_im(3) << ", " << q_im(0) << ", " << q_im(1) << ", " << q_im(2);
+
+    return os.str();
+  }
 };
 }
 #endif  // MAG_SENSOR_STATE_TYPE_H
