2d2dd47515027612216331ff559dadd439af83c4
Jantos, Thomas Georg <thomas.jantos@aau.at>
2022-08-12 09:16:44 +0200

    feat: added measurement base class and interface

---
+++ source/mars/include/mars/sensors/measurement_base_class.h
@@ -0,0 +1,32 @@
+// Copyright (C) 2021 Christian Brommer, Control of Networked Systems, University of Klagenfurt, Austria.
+//
+// All rights reserved.
+//
+// This software is licensed under the terms of the BSD-2-Clause-License with
+// no commercial use allowed, the full terms of which are made available
+// in the LICENSE file. No license in patents is granted.
+//
+// You can contact the author at <christian.brommer@ieee.org>
+
+
+#ifndef MEASUREMENT_BASE_CLASS_H
+#define MEASUREMENT_BASE_CLASS_H
+
+#include <mars/sensors/measurement_interface.h>
+#include <Eigen/Dense>
+
+namespace mars
+{
+class BaseMeas : Public MeasInterface
+{
+public:
+    Eigen:MatrixXd meas_noise;
+    bool has_meas_noise{False};
+
+    BaseMeas()
+    {
+    }
+};
+}
+
+#endif // MEASUREMENT_BASE_CLASS_H
---
+++ source/mars/include/mars/sensors/measurement_interface.h
@@ -0,0 +1,38 @@
+// Copyright (C) 2021 Christian Brommer, Control of Networked Systems, University of Klagenfurt, Austria.
+//
+// All rights reserved.
+//
+// This software is licensed under the terms of the BSD-2-Clause-License with
+// no commercial use allowed, the full terms of which are made available
+// in the LICENSE file. No license in patents is granted.
+//
+// You can contact the author at <christian.brommer@ieee.org>
+
+#ifndef MEASUREMENT_INTERFACE_H
+#define MEASUREMENT_INTERFACE_H
+
+#include <Eigen/Dense>
+
+namespace mars
+{
+class MeasInterface()
+{
+    public:
+
+    MeasInterface()
+    {
+    }
+
+    ///
+    /// \brief get the measurement noise associated with the current sensor measurement
+    /// \param sensor_data contains the current sensor measurement
+    /// \return Measurment noise matrix
+    ///
+    virtual Eugen::MatrixXd get_meas_noise(const std::shared_ptr<void>& sensor_data) = 0;
+
+    protected:
+
+};
+}
+
+#endif // MEASUREMENT_INTERFACE_H
