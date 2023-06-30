1a245132c9a3633df1c71cd10e1234d2f2ea78a1
Christian Brommer <christian.brommer@aau.at>
2023-06-30 11:32:20 +0200

    maint: update docker image to use ubuntu 22.04
    and docker_application_test.sh now removes a possibly existing build
    folder before running a clean build process.

--- deploy/scripts/docker_application_test.sh
+++ deploy/scripts/docker_application_test.sh
@@ -5,6 +5,7 @@ cd test
 echo "Delete temporary test files to enforce unzip of original data..."
 rm -rf source/tests/test_data
 echo "Building the Appliaction..."
+rm -rf build
 mkdir build && cd build
 cmake ../ && make -j
 echo "Running Google Tests"
